14a6877e48ce24c2c182d17924b00876f57f8671
mateusntn <mateusantoniodev@gmail.com>
2021-06-26 23:44:46 -0300

    Creates route to modal

--- src/route.js
+++ src/route.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const QuestionController = require('./controllers/QuestionController');
 const route = express.Router();
 
 route.get('/', (req, res) => res.render("index"));
 route.get('/create-pass', (req,res ) => res.render("create-pass"));
 route.get('/room', (req,res ) => res.render("room"));
 
-module.exports = route
+route.post('/room/:room/:question/:action', QuestionController.index);
+
+module.exports = route;
--- src/server.js
+++ src/server.js
@@ -7,6 +7,7 @@ server.set('view engine', 'ejs');
 server.set('views', path.join(__dirname, 'views'));
 
 server.use(express.static('public'));
+server.use(express.urlencoded({extended: true}));
 server.use(route);
 
 server.listen(3000, () => {
