58543d7ab177e418e2b7d1bb37f15063222f119f
mateusntn <mateusantoniodev@gmail.com>
2021-06-27 00:24:18 -0300

    Creates partials for index

--- src/route.js
+++ src/route.js
@@ -2,8 +2,8 @@ const express = require('express');
 const QuestionController = require('./controllers/QuestionController');
 const route = express.Router();
 
-route.get('/', (req, res) => res.render("index"));
-route.get('/create-pass', (req,res ) => res.render("create-pass"));
+route.get('/', (req, res) => res.render("index", {page: "enter-room"}));
+route.get('/create-pass', (req,res ) => res.render("index", {page: "create-pass"}));
 route.get('/room', (req,res ) => res.render("room"));
 
 route.post('/room/:room/:question/:action', QuestionController.index);
