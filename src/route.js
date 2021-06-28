448373e3df18fea7951aa7704ba6db504ca33502
mateusntn <mateusantoniodev@gmail.com>
2021-06-28 10:37:16 -0300

    Creates a route and function to pass the roomId to the views

--- src/route.js
+++ src/route.js
@@ -5,9 +5,11 @@ const route = express.Router();
 
 route.get('/', (req, res) => res.render("index", {page: "enter-room"}));
 route.get('/create-pass', (req,res ) => res.render("index", {page: "create-pass"}));
-route.get('/room/:room', (req,res ) => res.render("room"));
+
+route.get('/room/:room', RoomController.open);
+route.post('/create-room', RoomController.create);
+
 
 route.post('/question/:room/:question/:action', QuestionController.index);
-route.post('/create-room', RoomController.create);
 
 module.exports = route;
